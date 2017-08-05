049b99c349f42fbb3bda26f1d6c9a0efa62d021a
gaoshuaipeng <a112121788@163.com>
2017-08-05 15:37:30 +0800

    update unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -45,14 +45,14 @@ end
 timeout 1200
 
 # feel free to point this anywhere accessible on the filesystem
-pid app_path+"/tmp/pids/unicorn_nowapp.pid"
+pid app_path+"/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path app_path+"/log/unicorn_nowapp.stderr.log"
+stderr_path app_path+"/log/unicorn.stderr.log"
 
-stdout_path app_path+"/log/unicorn_nowapp.stdout.log"
+stdout_path app_path+"/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
