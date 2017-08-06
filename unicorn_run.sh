acc952b9b685947876f41e24affca8d43c4fc925
gaoshuaipeng <a112121788@163.com>
2017-08-06 12:24:31 +0800

    no message

--- unicorn_run.sh
+++ unicorn_run.sh
@@ -14,8 +14,8 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT="$1/current"
-PID="$1/shared/unicorn.pid"
+APP_ROOT="$2/current"
+PID="$2/shared/unicorn.pid"
 CMD="/Users/dongjunjun/.rbenv/shims/bundle exec unicorn_rails -D -c $APP_ROOT/config/unicorn.rb  -E development"
 UPGRADE_DELAY=${UPGRADE_DELAY-2}
 action="$1"
